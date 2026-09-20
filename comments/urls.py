771c298a4e71c54714961681501ffd697b938c18
Pratham <bhokarepratham2@gmail.com>
2026-09-20 23:02:04 +0530

    Update URL endpoints

--- comments/urls.py
+++ comments/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path("Comment/<int:pk>/", views.CommentAPIView.as_view() ),
+    path("comment/<int:pk>/", views.CommentAPIView.as_view() ),
 
     # For List Comment
-    path("Comments/<int:pk>/", views.CommentsDetailedAPIView.as_view()),
+    path("comments/<int:pk>/", views.CommentsDetailedAPIView.as_view()),
     path("update/<int:pk>/", views.CommentUpdateView.as_view())
 ]
