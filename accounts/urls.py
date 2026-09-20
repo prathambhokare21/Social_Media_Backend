826c84958c042472ec13960764daaf328b8d35f9
Pratham <bhokarepratham2@gmail.com>
2026-09-20 22:42:40 +0530

    update URL endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from accounts import views
 urlpatterns = [
     path('register/', views.RegistrationAPIView.as_view()),
-    path('Login/', views.LoginAPIView.as_view()),
-    path('Profile/', views.ProfileAPIView.as_view()),
-    path('Follow/<int:user_id>/', views.FollowAPIView.as_view()),
+    path('login/', views.LoginAPIView.as_view()),
+    path('profile/', views.ProfileAPIView.as_view()),
+    path('follow/<int:user_id>/', views.FollowAPIView.as_view()),
     path('unfollow/<int:user_id>/', views.UnfollowAPIView.as_view()),
 ]
