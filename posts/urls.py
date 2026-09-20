571ee59ee42748c2bab463e1835280e89b23032f
Pratham <bhokarepratham2@gmail.com>
2026-09-20 23:04:30 +0530

    Update URL endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -3,11 +3,11 @@ from posts import views
 
 urlpatterns = [
     path("create/", views.PostAPIView.as_view()),
-    path("List/", views.PostListAPIView.as_view()),
+    path("list/", views.PostListAPIView.as_view()),
     path("feed/", views.UserFeedAPIView.as_view()),
 
     # To get individiual data
 
-    path("List/<int:pk>/", views.PostListAPIDetailedView.as_view()),
-    path("Like/<int:pk>/", views.PostLikeAPIView.as_view()),
+    path("list/<int:pk>/", views.PostListAPIDetailedView.as_view()),
+    path("like/<int:pk>/", views.PostLikeAPIView.as_view()),
 ]
